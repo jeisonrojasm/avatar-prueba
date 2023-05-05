d0d5113a5ebe577d51223e27bca574e60754f20e
Jeison Rojas <jeison.rojas@Ci2.local>
2023-05-05 10:58:42 -0500

    Ahora traigo el código del componente gltf-model desde el archivo minificado del cdn de aframe que estoy susando actualmente. Este código SÍ funciona para cargar el modelo glb en la entidad

--- pruebas.js
+++ pruebas.js
@@ -1,21 +1,28 @@
-AFRAME.registerComponent('gltf-model', {
+AFRAME.registerComponent("modelo-gltf", {
     schema: {
-        src: { type: 'asset' },
-        crossorigin: { default: '' }
+        type: "model"
     },
-
     init: function () {
-        this.loaderModelo = new THREE.GLTFLoader();
-        this.model = null;
-        this.materials = null;
-
-        // Wait for model to load.
-        this.loaderModelo.load(this.data.src, function gltfLoaded(modelo) {
-            this.model = modelo.scene || modelo.scenes[0];
-            this.el.setObject3D('mesh', this.model);
-            this.el.emit('model-loaded', { format: 'gltf', model: this.model });
-        }, undefined, function gltfFailed(error) {
-            console.error(error);
-        });
+        this.model = null, this.loader = new THREE.GLTFLoader
+    },
+    update: function () {
+        var e = this,
+            o = this.el,
+            t = this.data;
+        t && (this.remove(), this.loader.load(t, function (t) {
+            e.model = t.scene || t.scenes[0], e.model.animations = t.animations, o.setObject3D("mesh", e.model), o.emit("model-loaded", {
+                format: "gltf",
+                model: e.model
+            })
+        }, void 0, function (e) {
+            var r = e && e.message ? e.message : "Failed to load glTF model";
+            warn(r), o.emit("model-error", {
+                format: "gltf",
+                src: t
+            })
+        }))
+    },
+    remove: function () {
+        this.model && this.el.removeObject3D("mesh")
     }
 });
